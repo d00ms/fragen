104a76bdb56dc0211d0ee1eff6d798b006b1bb74
Sakti Reysaputra <reysaputrasakti@gmail.com>
2013-06-18 19:01:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fragen::Application.config.secret_token = 'e64d8fc05f276cac7cc80a411c2990bf41d90ff62606a3118209b8e0b803dbb04f62efaa99be923a81b5e6ea9f1831462da28ca72eaf785b4376fc0040a308c5'
